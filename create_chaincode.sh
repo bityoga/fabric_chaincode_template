35c15901ad3a38e035d4199195eb422b9b4ed3af
anandhakumarpalanisamy <anandhakumarpalanisamy91@gmail.com>
2020-11-25 21:44:00 +0100

    - updated chaincode create script and util index

--- create_chaincode.sh
+++ create_chaincode.sh
@@ -2,6 +2,7 @@
 set -x #echo on
 ​
 CHAINCODE_NAME="transaction"
+CHAINCODE_CLASS_NAME="TransactionChaincode"
 
 platform='unknown'
 unamestr=$(uname)
@@ -13,10 +14,12 @@ fi
 
 if [ "$platform" = 'linux' ]; then
 
-   find ./chaincode/ -type f -exec sed -i "s/CHAINCODE-NAME/$CHAINCODE_NAME/g" {} \;
+   find ./chaincode/ -type f -exec sed -i "s/CHAINCODE-NAME/$CHAINCODE_NAME/g" {} \; && 
+   find ./chaincode/ -type f -exec sed -i "s/CHAINCODE_CLASS_NAME/$CHAINCODE_CLASS_NAME/g" {} \;
 
 elif [ "$platform" = 'freebsd' ]; then
 
-  find ./chaincode/ -type f -exec sed -i '.bak' "s/CHAINCODE-NAME/$CHAINCODE_NAME/g" {} \;
+  find ./chaincode/ -type f -exec sed -i '.bak' "s/CHAINCODE-NAME/$CHAINCODE_NAME/g" {} \;&&
+  find ./chaincode/ -type f -exec sed -i '.bak' "s/CHAINCODE_CLASS_NAME/$CHAINCODE_CLASS_NAME/g" {} \;
 
 fi
